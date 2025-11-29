51fea11ff93276babc16e3ef2530ffd8eb9c63a0
micrillion <tiffanylward76@gmail.com>
2025-11-28 19:12:35 -0500

    nerfed dilation+ study requirements by 50% except td8 which is 90%, made reality cost 5e8

--- src/core/secret-formula/eternity/time-studies/dilation-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/dilation-time-studies.js
@@ -2,7 +2,7 @@ export const dilationTimeStudies = [
   {
     id: 1,
     description: "Unlock Time Dilation",
-    cost: 5000,
+    cost: 4500,
     requirement: () => {
       const ttRequirement = Currency.timeTheorems.max.gte(TimeStudy.dilation.totalTimeTheoremRequirement);
       if (Ra.unlocks.autoUnlockDilation.canBeApplied &&
@@ -20,25 +20,25 @@ export const dilationTimeStudies = [
   {
     id: 2,
     description: "Unlock the 5th Time Dimension",
-    cost: 1e6,
+    cost: 5e5,
     requirement: () => PlayerProgress.dilationUnlocked()
   },
   {
     id: 3,
     description: "Unlock the 6th Time Dimension",
-    cost: 1e7,
+    cost: 5e6,
     requirement: () => TimeStudy.timeDimension(5).isBought
   },
   {
     id: 4,
     description: "Unlock the 7th Time Dimension",
-    cost: 1e8,
+    cost: 5e7,
     requirement: () => TimeStudy.timeDimension(6).isBought
   },
   {
     id: 5,
     description: "Unlock the 8th Time Dimension",
-    cost: 1e9,
+    cost: 1e8,
     requirement: () => TimeStudy.timeDimension(7).isBought
   },
   {
@@ -46,7 +46,7 @@ export const dilationTimeStudies = [
     description: () => (Pelle.isDoomed
       ? "You cannot escape a Doomed Reality"
       : "Unlock Reality"),
-    cost: 1,
+    cost: 5e8,
     requirement: () => TimeStudy.timeDimension(8).isBought &&
       player.records.thisReality.maxEP.exponent >= 4000 &&
       (Perk.firstPerk.isBought || Achievements.preReality.every(a => a.isUnlocked)) &&
